ed1d1442b4a887cd97be35ab8165a7cc7acf6415
Zietieflr <lrmcguire93@gmail.com>
2020-07-14 15:13:05 -0600

    Lesson day 1.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BagelsContainer from './Components/BagelsContainer'
+import Form from './Components/Form'
+
+class App extends React.Component {
+  state = {
+    bagels: ['bagel 1', 'bagel 2']
+  }
+  
+  render() {
+    return (
+      <>
+        <h1>This is Our bagels App</h1>
+        <Form />
+        <BagelsContainer />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/BagelsContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Delete from './Delete'
+import Bagel from './Bagel'
+
+const BagelsContainer = () => {
+  return (
+    <>
+      <h3>This is the BagelsContainer</h3>
+      <div>
+        <Delete />
+        <Bagel />
+      </div>
+    </>
+  )
+}
+
+export default BagelsContainer
